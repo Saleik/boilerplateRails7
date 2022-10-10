5c2a1fefaea6144ce8afb6793867a9709a39731b
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-10-10 16:08:31 +0200

    [+][init]first commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_tree ../builds
+//= link controllers/application.js
+
+//= link_tree ../../javascript .js
+//= link application.js
---
+++ app/javascript/application.js
@@ -0,0 +1,5 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import 'bootstrap';
+import * as bootstrap from 'bootstrap';
+import 'controllers';
+import '@hotwired/turbo-rails';
